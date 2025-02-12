00bb7b04a9bbc41152dd3b8f73187b1d9db9597c
its_e.n.o.c.k <mutetienock4@gmail.com>
2025-02-12 12:26:22 +0300

    update vite  config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Define the output directory
+    chunkSizeWarningLimit: 1000, // Adjust chunk size warning limit
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 })
